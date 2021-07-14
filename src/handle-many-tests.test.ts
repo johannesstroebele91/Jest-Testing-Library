7fd844e4ca26baaa1664e78f2687b1e9bb3674a6
Johannes Stroebele (ext.) <Johannes.Stroebele@partner.bmwgroup.com>
2021-07-14 11:26:00 +0200

    Add explanation for handling many tests simultaneously

---
+++ src/handle-many-tests.test.ts
@@ -0,0 +1,67 @@
+// Ref: https://jestjs.io/docs/setup-teardown
+
+// 1) beforeEach / afterEach
+// If you have some work you need to do repeatedly for many tests, you can use beforeEach and afterEach
+// method initializeCityDatabase() that must be called before each of these tests
+beforeEach(() => {
+  initializeCityDatabase();
+});
+/* Alternative for async
+beforeEach(() => {
+  return initializeCityDatabase();
+});*/
+
+afterEach(() => {
+  clearCityDatabase();
+});
+
+test("city database has Vienna", () => {
+  expect(isCity("Vienna")).toBeTruthy();
+});
+
+test("city database has San Juan", () => {
+  expect(isCity("San Juan")).toBeTruthy();
+});
+
+// 2) Scoping (grouping)
+// Applies to all tests in this file
+beforeEach(() => {
+  return initializeCityDatabase();
+});
+
+test("city database has Vienna", () => {
+  expect(isCity("Vienna")).toBeTruthy();
+});
+
+test("city database has San Juan", () => {
+  expect(isCity("San Juan")).toBeTruthy();
+});
+
+describe("matching cities to foods", () => {
+  // Applies only to tests in this describe block
+  beforeEach(() => {
+    return initializeFoodDatabase();
+  });
+
+  test("Vienna <3 veal", () => {
+    expect(isValidCityFoodPair("Vienna", "Wiener Schnitzel")).toBe(true);
+  });
+
+  test("San Juan <3 plantains", () => {
+    expect(isValidCityFoodPair("San Juan", "Mofongo")).toBe(true);
+  });
+});
+
+// Order of execution (illustrated)
+beforeAll(() => console.log("1 - beforeAll"));
+afterAll(() => console.log("1 - afterAll"));
+beforeEach(() => console.log("1 - beforeEach"));
+afterEach(() => console.log("1 - afterEach"));
+test("", () => console.log("1 - test"));
+describe("Scoped / Nested block", () => {
+  beforeAll(() => console.log("2 - beforeAll"));
+  afterAll(() => console.log("2 - afterAll"));
+  beforeEach(() => console.log("2 - beforeEach"));
+  afterEach(() => console.log("2 - afterEach"));
+  test("", () => console.log("2 - test"));
+});
